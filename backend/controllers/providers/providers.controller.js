75444175ea7dd062ee11b56538ff43b2e79fd48a
mnesfane <abdessalamnesfane@gmail.com>
2025-02-26 15:27:18 +0000

    create provider and delete edit / bug in edit METH PUT

--- backend/controllers/providers/providers.controller.js
+++ backend/controllers/providers/providers.controller.js
@@ -2,19 +2,30 @@ import mongoose  from "mongoose";
 import Provider  from '../../models/Providers/provider.model.js';
 
 export const Insertprovider = async (req, res, next)=>{
+    
+    // if (!req.file) {
+    //     return res.status(400).json({ success: false, error: 'No file uploaded or file type not supported' });
+    // }
+    
     const mongoSession = await mongoose.startSession();
-
+    
     try {
-        const newProvider = new Provider(req.body);
-        // 
+        const newProvider = new Provider({
+            name: req.body.name,
+            category : req.body.category,
+            website : req.body.website,
+        });
+        if(req.file){
+            newProvider.logo = req.file.path
+        }
+        
         await newProvider.save({ session: mongoSession });
         // await mongoSession.commitTransaction();
-        res.status(201).send(newProvider);
+        res.status(201).send({message: 'emp added successfully'});
     } catch (error) {
         // await mongoSession.abortTransaction();
         next(error);
     }
-
     // console.log('Request Method:', req.method);
     // console.log('Request URL:', req.url);
     // console.log('Request Headers:', req.headers);
@@ -32,4 +43,55 @@ export const showProviders = async (req, res, next)=>{
     }catch(err){
         next(err)
     }
+}
+
+export const deleteProvider = async(req, res, next) =>{
+    try{
+        const providerExist = await Provider.findById(req.params.id)
+        if(!providerExist){
+            return res.status(404).json({ message: 'Provider not found' })
+        }
+        Provider.findByIdAndDelete(req.params.id).then(res.status(200).json({ message: 'Provider deleted successfully' }))
+    }catch(err){
+        next(err)
+    }
+}
+
+export const showEditProviderPage = async(req, res, next) =>{
+    try {
+        // returns the provider
+        const providerExist = await Provider.findById(req.params.id);
+        if (!providerExist) {
+            return res.status(404).json({ message: 'Provider not found' });
+        }
+        return res.status(200).json(providerExist);
+    }catch(err){
+        next(err)
+    } 
+}
+
+
+export const editProvider = async(req, res, next) =>{
+    const providerExist = await Provider.findById(req.params.id)
+    if(!providerExist){
+        return res.status(404).json({ message: 'Provider not found' })
+    }
+    const updatedProvider = {
+        name: req.body.name,
+        category : req.body.category,
+        website : req.body.website,
+        logo : req.body.logo
+    }
+    console.log(req.body.name)
+    return res.status(200).json({
+        success: true, 
+        message: 'Provider is updated', 
+        data: updatedProvider
+    })
+
+    Provider.findByIdAndUpdate(req.params.id, updatedProvider, {new: true}).then((newUpdatedProvider) => res.status(200).json({
+        success: true, 
+        message: 'Provider is updated', 
+        data: newUpdatedProvider
+    }))
 }
--- backend/routes/providers/providers.routes.js
+++ backend/routes/providers/providers.routes.js
@@ -1,11 +1,25 @@
 import { Router } from 'express';
-import {Insertprovider, showProviders} from '../../controllers/providers/providers.controller.js'
+import {Insertprovider, showProviders, deleteProvider, editProvider, showEditProviderPage} from '../../controllers/providers/providers.controller.js'
+import upload from '../../middleware/upload.middleware.js'
 
 const providerRoute = Router()
 
-providerRoute.post('/providers/create', Insertprovider)
+// providerRoute.post('/providers/create', upload.single('logo'), Insertprovider);
+providerRoute.post('/providers/create', upload.single('logo'), (req, res) =>{
+    console.log(req.file);
+    if (!req.file) {
+      return res.status(400).json({ error: 'File upload failed' });
+    }
+    res.json({ message: 'Upload successful', file: req.file });
+});
 
 providerRoute.get('/providers', showProviders)
+ 
+providerRoute.delete('/providers/delete/:id', deleteProvider)
+
+providerRoute.get('/providers/edit/:id', showEditProviderPage)
+
+providerRoute.put('/providers/edit/:id', editProvider)
 
 
 export default providerRoute
