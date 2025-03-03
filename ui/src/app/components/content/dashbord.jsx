6c69b98dec6cf32120c15c4993a154cf986e98d2
Mouhssine Kinani <kinanimouhssine@gmail.com>
2025-03-03 23:39:01 +0000

    layout v0.2.3

---
+++ ui/src/app/components/content/dashbord.jsx
@@ -0,0 +1,35 @@
+export default function Content({ children }) {
+    return (
+        <main className="bg-gray-500 p-4 area-content">
+        {children}
+        <div className="">
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          lorem lorem           lorem lorem 
+          <br/>
+          <br/>
+          <br/>
+        </div>
+      </main>
+    );
+  }
+  
--- ui/src/app/layout.js
+++ ui/src/app/layout.js
@@ -1,57 +1,22 @@
 'use client'
 import "./globals.css";
+import Header from "./components/header/Header";
+import SideBar from "./components/sidebar/SideBar";
+import Content from "./components/content/dashbord";
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
       <body className="grid-areas-layout">
         {/* Sidebar */}
-        <aside className="bg-gray-800 text-white p-4 area-sidebar">
-          Sidebar
-        </aside>
+        <SideBar/>
 
         {/* Header */}
-        <header className="bg-blue-500 text-white p-4 area-header">
-          Header
-          <div
-            class="d-none d-sm-block"
-          >
-            Some Word
-          </div>
-          
-        </header>
+          <Header/>
 
         {/* Content */}
-        <main className="bg-gray-500 p-4 area-content">
-          {children}
-          <div className="">
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            lorem lorem           lorem lorem 
-            <br/>
-            <br/>
-            <br/>
-          </div>
-        </main>
+        <Content/>
+       
       </body>
     </html>
   );
