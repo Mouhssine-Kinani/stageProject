4351074a83fed74c4ae0829511752e0a6979ca35
Mouhssine Kinani <kinanimouhssine@gmail.com>
2025-03-05 13:41:08 +0000

    navigation with sidebar is done v2

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -1,7 +1,6 @@
-export default function Page() {
-  return (
-    <div className="content">
-      Content
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/home"); // Redirect root to /home
+  return null; // This component won't render
 }
