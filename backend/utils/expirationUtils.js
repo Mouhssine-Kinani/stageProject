2ea9dcb8148609c52a3d7a63622258b0062f0348
mnesfane <abdessalamnesfane@gmail.com>
2025-03-20 11:08:59 +0000

    email notification (reminder)

---
+++ backend/utils/expirationUtils.js
@@ -0,0 +1,23 @@
+const calculateExpirationDate = (product) => {
+  const deploymentDate = new Date(product.productDeployed);
+  const cycles = {
+    monthly: 1,
+    yearly: 12,
+    biennial: 24
+  };
+
+  const months = cycles[product.billing_cycle];
+  const expirationDate = new Date(deploymentDate);
+  expirationDate.setMonth(expirationDate.getMonth() + months);
+  
+  return expirationDate;
+};
+
+const getRemainingDays = (expirationDate) => {
+  const today = new Date();
+  const expDate = new Date(expirationDate);
+  const diffTime = expDate - today;
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+};
+
+export { calculateExpirationDate, getRemainingDays }; 
